89fcf7b0482ce0ccaeb778325333064bd087098f
SEUNGHOON LEE <kneeprayer@gmail.com>
2016-09-15 15:15:58 +0800

    add new project

---
+++ CheckIO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckIO
+//
+//  Created by SEUNGHOON LEE on 9/15/16.
+//  Copyright © 2016 kneeprayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
